7eed336f80d6a659c52f4bb2d9295cc92f84580d
PavloSnieda <kvip35125@gmail.com>
2023-01-19 15:00:45 -0800

    start

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import App from './App';
+import App from './App.tsx'
+
+
+const element = document.getElementById('root')
+
+if (element) {
+  const root = ReactDOM.createRoot(element);
+
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+}
+
+
+
